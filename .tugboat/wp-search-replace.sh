8ee4ef57f72aec876df3f36245cdff011284b11a
Ben Chavet <ben.chavet@lullabot.com>
2018-10-26 12:01:44 -0500

    use wp-cli to configure wordpress

--- .tugboat/wp-search-replace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "x${TUGBOAT_BASE_PREVIEW}" != "x" ]; then
-    wp --allow-root --path="${DOCROOT}" search-replace "${TUGBOAT_BASE_PREVIEW_URL_HOST}" "${TUGBOAT_SERVICE_URL_HOST}" --skip-columns=guid
-else
-    wp --allow-root --path="${DOCROOT}" search-replace 'wordpress.local' "${TUGBOAT_SERVICE_URL_HOST}" --skip-columns=guid
-fi
